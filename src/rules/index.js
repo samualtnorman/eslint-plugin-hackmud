58bd3186d6863c64ad8bf01939826a8f584ca747
Samual Norman <samualtnorman@gmail.com>
2020-09-01 22:44:17 +0100

    enabled checking typescript files disabled no-closure-siblings rule to prevent crash with typescript plugin (see https://github.com/eslint/eslint/issues/13639)

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,9 +1,9 @@
 import noClosureIdentifier from './no-closure-identifier'
-import noClosureSiblings from './no-closure-siblings'
+// import noClosureSiblings from './no-closure-siblings'
 import validateSubscriptSyntax from './validate-subscript-syntax'
 
 export default {
   'no-closure-identifier': noClosureIdentifier,
-  'no-closure-siblings': noClosureSiblings,
+  // 'no-closure-siblings': noClosureSiblings,
   'validate-subscript-syntax': validateSubscriptSyntax
 }
